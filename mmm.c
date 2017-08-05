cd680e85134f0599b4d946c8476fe7c7888562eb
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2017-08-05 16:53:16 +0530

    mmm
    this is code

---
+++ mmm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("the value of a");
+scanf("%d",&a);
+if(a%2==0)
+printf("the value is even");
+else
+printf("the value is odd");
+getch();
+}
