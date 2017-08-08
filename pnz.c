eac615e2a342b8364847daaa0677876575d62ad0
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2017-08-08 15:51:33 +0530

    pnz.c

---
+++ pnz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+float number;
+    clrscr();
+    printf("Enter a number:");
+    scanf("%f",number);
+    if{number==0)
+    {
+        printf("The Number is 0");
+    }
+       else if(number>0)
+       {
+           print("The Number is positive");
+       }
+       else
+       {
+           printf("The Number is Negative");
+       }
+       getch();
+      }
+       
