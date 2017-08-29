840e84a019f63620b18be4f5cba396dbb2381ae1
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2017-08-29 15:02:14 +0530

    alphabet or not
    My C Program

---
+++ alpha.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#inlcude<conio.h>
+int main()
+{
+char alpha;
+printf("Enter a character: ");
+scanf("%c",&alpha);
+if( (alpha>='a' && alpha<='z') || (alpha>='A' && alpha<='Z') )
+printf("%c is an alphabet.",alpha);
+else
+printf("%c is not an alphabet.",alpha);
+getch();
+}
