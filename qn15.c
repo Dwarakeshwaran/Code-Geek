37ce38ed497e99c3b07ade943acebce5bb3410d9
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-01-31 16:14:26 +0530

    Even Numbers between the range

---
+++ qn15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2,i,odd;
+
+printf("Enter the 1st Number:\t");
+scanf("%d",&num1);
+printf("Enter the 2nd Number:\t");
+scanf("%d",&num2);
+
+for(i=num1+1;i<num2;i++)
+{
+if(i%2==0)
+printf("%d\n",i);
+}
+
+}
