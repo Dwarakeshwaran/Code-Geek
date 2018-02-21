2cf7bba654fd6bbd9c6fe7150b6e2709b9ed3734
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-02-21 16:33:55 +0530

    SOrting array with improved constraints

---
+++ qn26.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int size,i,j,n[size],val;
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&n[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		for(j=i+1;j<size;j++)
+		{
+			if(n[i]>n[j])
+			{
+				val=n[j];
+				n[j]=n[i];
+				n[i]=val;
+			}
+		}
+		printf("%d\n",n[i]);		
+	}		
+}
