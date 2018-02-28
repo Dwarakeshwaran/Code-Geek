aa2f400499994dc5e15ec32603655a63186663e5
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-02-28 16:09:00 +0530

    Update qn46.c

--- qn46.c
+++ qn46.c
@@ -5,8 +5,12 @@ void main()
     int num;
     
     scanf("%d",&num);
+    while(num<=1000000)
+          {
+              num++;
+              break;
+          }
     
-    num++;
     
     printf("%d",num);
 }
