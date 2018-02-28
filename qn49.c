75a0c7c0f09c32db4b4eba736014f97fb8cb5b15
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-02-28 16:53:14 +0530

    Sum of two numbers using array

---
+++ qn49.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+   int num[100],sum;
+   
+   
+   scanf("%d",&num[0]);
+   scanf("%d",&num[1]);
+   
+   sum=num[0]+num[1];
+  
+   printf("%d",sum);
+   
+  
+}
