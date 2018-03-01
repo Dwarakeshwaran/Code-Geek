0f804c4c9151b0fbba070ebc21a91be5bcc4c00c
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-03-01 01:23:31 -0800

    Sum of two number using arrays

--- qn49.c
+++ qn49.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
-
 void main()
 {
-   int num[100],sum;
-   
-   
-   scanf("%d",&num[0]);
-   scanf("%d",&num[1]);
-   
-   sum=num[0]+num[1];
-  
-   printf("%d",sum);
-   
-  
+    int num[50],i=1,sum=0;
+    
+    if(i<=1)
+    {
+    scanf("%d",&num[i]);
+    sum=sum+num[i];
+    i--;  
+    scanf("%d",&num[i]);
+    sum=sum+num[i];
+    }
+    
+    printf("%d",sum);
+    
 }
