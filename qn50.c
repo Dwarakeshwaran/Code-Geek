da4b072ba3dd34258d84d806fd80120871cd26f2
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-03-02 15:53:39 +0530

    power of 2

---
+++ qn50.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int num,root;
+    
+    scanf("%f",&num);
+    
+    root=sqrt(num);
+    
+    if(root%2==0)
+    printf("yes");
+    else
+    printf("no");
+    
+    
+}
