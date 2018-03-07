21afbe2c4cf1348601173363b254e1cda0be1aa5
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-03-07 15:49:05 +0530

    Nearest even Number

--- qn54.c
+++ qn54.c
@@ -1,16 +1,11 @@
 #include<stdio.h>
-
 void main()
 {
-    int num;
-    
-    scanf("%d",&num);
-    
-    if(num%2==0)
-    printf("%d",num);
-    else if(num%2!=0)
-    {
-        num=num-1;
-        printf("%d",num);
-    }
+	int num,i;
+	scanf("%d",&num);
+	
+	if(num%2==0)
+	printf("%d",num);
+	else
+	printf("%d",num-1);
 }
