3fe136fcc26ac18993abf67b4a48da455ca05f49
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-03-07 15:31:36 +0530

    Create qn51.c

---
+++ qn51.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+int a,b;//Variable Declarations
+printf("Enter the First Number: ");
+scanf("%d",&a);
+printf("Enter the Second Number: ");
+scanf("%d",&b);
+printf("The sum of the Numbers is %d",a+b);//Directly added the sum using the formula inside the printf statement
+
+return 0;
+
+}
+d3q1.c
+Displaying d3q1.c.
