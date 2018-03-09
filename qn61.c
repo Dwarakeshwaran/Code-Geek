83fd08035ff1b93a61424bad531788e5b1e5f060
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-03-09 15:39:25 +0530

    Number of characters in a string to br displayed

---
+++ qn61.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+    char a[10];
+    int i,n;
+    printf("Enter the string: ");
+    scanf("%s",&a);
+    printf("Enter the number of letters to be displayed: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%c",a[i]);
+    }
+}
