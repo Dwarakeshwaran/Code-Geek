b646cde3afcfe74b472db61523b48075c3cd7151
Dwarakeshwaran <30549378+Dwarakeshwaran@users.noreply.github.com>
2018-03-12 16:43:52 +0530

    Product of number is Perfect Square

--- qn79.c
+++ qn79.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
-//#include<math.h>
+
 void main()
 {
-	int n1,n2,i,j,square,res,temp;
+	int n1,n2,i,square,res,temp;
+	
 	scanf("%d",&n1);
 	scanf("%d",&n2);
 	
 	res=n1*n2;
 	
-	for(i=0;i<1000;i++)
+	for(i=0;i<=10000;i++)
 	{
-		for(j=0;j<1000;j++)
-		{
-			square=i*j;
+		
+			square=i*i;
 			if(res==square)
 			temp=1;		
-		}
+		
 	}
 	if(temp==1)
 	printf("yes");
