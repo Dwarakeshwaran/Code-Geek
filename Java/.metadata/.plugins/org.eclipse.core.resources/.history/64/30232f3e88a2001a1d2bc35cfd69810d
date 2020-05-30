98c5823ae795a3e7a31a764c28ee6ea3ed4e60d0
dm826p <dm826p@att.com>
2020-05-30 20:48:21 +0530

    Anime List project Added [Spring Boot]

---
+++ Java/.metadata/.plugins/org.eclipse.core.resources/.history/64/30232f3e88a2001a1d2bc35cfd69810d
@@ -0,0 +1,24 @@
+package com.spring.restful;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimeService {
+	
+	public static List<Anime> animeList = new ArrayList<Anime>();
+	
+	static {
+		animeList.add(new Anime("DBZ","Action",8));
+		animeList.add(new Anime("OPM","Comedy",9));
+		animeList.add(new Anime("Gantz","Mystery",10));
+	}
+	
+	public List<Anime> getAnimeList() {
+		return animeList;
+	}
+	
+
+}
---
+++ Java/.metadata/.plugins/org.eclipse.egit.ui/egit3937781064938741273/8703106_Anime.java
@@ -0,0 +1,52 @@
+package com.spring.restful;
+
+public class Anime {
+	
+	public String name;
+	public String genre;
+	public int rating;
+	
+	public Anime() {
+		super();
+	}
+
+	public Anime(String name, String genre, int rating) {
+		super();
+		this.name = name;
+		this.genre = genre;
+		this.rating = rating;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		return "Anime [name=" + name + ", genre=" + genre + ", rating=" + rating + "]";
+	}
+	
+	
+	
+
+}
