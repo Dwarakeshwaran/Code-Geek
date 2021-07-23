6dc330518fc640a91eda388ef5966ae7e9d706d8
Dwarakesh <dwaki98@gmail.com>
2021-07-23 15:40:07 +0530

    [LeetCode] AddTwoNumbers - MyLinkedList and ListNode Separated

--- Java/Payirchi/src/dwarki/exercise/java/leetcode/linkedlist/AddTwoNumbers.java
+++ Java/Payirchi/src/dwarki/exercise/java/leetcode/linkedlist/AddTwoNumbers.java
@@ -7,138 +7,17 @@ import java.util.Arrays;
 
 //Started @14:06 21-07-2021
 //Ended @16:10 21-07-2021
-class ListNode {
-
-	int val;
-	ListNode next;
-
-	public ListNode() {
-		super();
-	}
-
-	public ListNode(int data) {
-		super();
-		this.val = data;
-	}
-
-	public ListNode(int data, ListNode node) {
-		super();
-		this.val = data;
-		this.next = node;
-	}
-
-}
-
-class LinkedList {
-
-	ListNode headNode;
-
-	public LinkedList() {
-		super();
-	}
-
-	public LinkedList(ListNode headNode) {
-		super();
-		this.headNode = headNode;
-	}
-
-	public void add(int number) {
-
-		ListNode duplicateNode = headNode;
-
-		if (headNode == null)
-			headNode = new ListNode(number);
-		else {
-			while (duplicateNode.next != null)
-				duplicateNode = duplicateNode.next;
-			duplicateNode.next = new ListNode(number);
-		}
-
-	}
-
-	public void print() {
-
-		ListNode duplicateNode = headNode;
-
-		if (headNode == null)
-			return;
-
-		while (duplicateNode.next != null) {
-			System.out.print(duplicateNode.val + " ");
-			duplicateNode = duplicateNode.next;
-		}
-		System.out.print(duplicateNode.val);
-		System.out.println(" ");
-	}
-
-	public void deleteLastNode() {
-
-		ListNode duplicateNode = headNode;
-		
-		if(headNode.next == null)
-		{
-			headNode = null;
-			return;
-		}
-			
-			
-		while (true) {
-
-			if (duplicateNode.next.next == null) {
-				duplicateNode.next = null;
-				return;
-			}
-			
-			duplicateNode = duplicateNode.next;
-		}
-
-	}
-
-	public ListNode getNode() {
-		return headNode;
-	}
-
-	public int goToEnd() {
-
-		ListNode duplicateNode = headNode;
-
-		if (headNode == null)
-			return -1;
-
-		while (duplicateNode.next != null) {
-			duplicateNode = duplicateNode.next;
-		}
-
-		return duplicateNode.val;
-	}
-
-	public int size() {
-
-		ListNode duplicateNode = headNode;
-
-		int count = 0;
-
-		while (duplicateNode.next != null) {
-			count++;
-			duplicateNode = duplicateNode.next;
-		}
-
-		return (count + 1);
-	}
-
-}
-
 public class AddTwoNumbers {
 
 	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-		String reversedNode1 = nodeReversal(new LinkedList(l1));
+		String reversedNode1 = nodeReversal(new MyLinkedList(l1));
 
-		String reversedNode2 = nodeReversal(new LinkedList(l2));
+		String reversedNode2 = nodeReversal(new MyLinkedList(l2));
 		
 		BigInteger sum = new BigInteger(reversedNode1).add(new BigInteger(reversedNode2));
 
-		LinkedList solutionNodeList = new LinkedList();
+		MyLinkedList solutionNodeList = new MyLinkedList();
 
 		for (int i = sum.toString().length() - 1; i >= 0; i--) {
 
@@ -146,15 +25,15 @@ public class AddTwoNumbers {
 			solutionNodeList.add(Integer.parseInt(String.valueOf(c)));
 		}
 
-		//solutionNodeList.print();
+		//solutionNodeList.show();
 
 		return solutionNodeList.getNode();
 
 	}
 
-	public static String nodeReversal(LinkedList list) {
+	public static String nodeReversal(MyLinkedList list) {
 
-		LinkedList checkNodeList = new LinkedList(list.getNode());
+		MyLinkedList checkNodeList = new MyLinkedList(list.getNode());
 
 		int endNodeData;
 		int count = list.size();
@@ -178,8 +57,8 @@ public class AddTwoNumbers {
 		int[] listArray1 = Arrays.stream(reader.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
 		int[] listArray2 = Arrays.stream(reader.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
 
-		LinkedList list1 = new LinkedList();
-		LinkedList list2 = new LinkedList();
+		MyLinkedList list1 = new MyLinkedList();
+		MyLinkedList list2 = new MyLinkedList();
 
 		for (int i = 0; i < listArray1.length; i++)
 			list1.add(listArray1[i]);
