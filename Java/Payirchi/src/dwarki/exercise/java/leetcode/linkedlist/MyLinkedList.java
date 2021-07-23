9b124d57ec419fd576901ea5d204c6a5bc4501f3
dm826p <dm826p@att.com>
2021-07-23 15:43:10 +0530

    [LeetCode] MyLinkedList Class Separated

---
+++ Java/Payirchi/src/dwarki/exercise/java/leetcode/linkedlist/MyLinkedList.java
@@ -0,0 +1,125 @@
+package dwarki.exercise.java.leetcode.linkedlist;
+
+class MyLinkedList {
+
+	ListNode headNode;
+
+	public MyLinkedList() {
+		super();
+	}
+
+	public MyLinkedList(ListNode headNode) {
+		super();
+		this.headNode = headNode;
+	}
+
+	public void add(int number) {
+
+		ListNode tempNode = headNode;
+
+		if (headNode == null)
+			headNode = new ListNode(number);
+		else {
+			while (tempNode.next != null)
+				tempNode = tempNode.next;
+			tempNode.next = new ListNode(number);
+		}
+
+	}
+
+	public void show() {
+
+		ListNode tempNode = headNode;
+
+		if (headNode == null)
+			return;
+
+		while (tempNode.next != null) {
+			System.out.print(tempNode.val + " ");
+			tempNode = tempNode.next;
+		}
+		System.out.print(tempNode.val);
+		System.out.println(" ");
+	}
+
+	public void deleteLastNode() {
+
+		ListNode tempNode = headNode;
+		
+		if(headNode.next == null)
+		{
+			headNode = null;
+			return;
+		}
+			
+			
+		while (true) {
+
+			if (tempNode.next.next == null) {
+				tempNode.next = null;
+				return;
+			}
+			
+			tempNode = tempNode.next;
+		}
+
+	}
+
+	public ListNode getNode() {
+		return headNode;
+	}
+
+	public int goToEnd() {
+
+		ListNode tempNode = headNode;
+
+		if (headNode == null)
+			return -1;
+
+		while (tempNode.next != null) {
+			tempNode = tempNode.next;
+		}
+
+		return tempNode.val;
+	}
+
+	public int size() {
+
+		ListNode tempNode = headNode;
+
+		int count = 0;
+
+		while (tempNode.next != null) {
+			count++;
+			tempNode = tempNode.next;
+		}
+
+		return (count + 1);
+	}
+	
+	public void removeAtPosition(int position) {
+
+		ListNode tempNode = headNode;
+
+		int count = 1;
+
+		if (count == position) {//This is for deleting the first Element
+			headNode = headNode.next;
+			return;
+		}
+
+		while (tempNode.next != null) {
+			if ((count + 1) == position) {
+				tempNode.next = tempNode.next.next;
+			}
+			if (count == position - 1)// This is for deleting the last element
+				return;// so that we can avoid the NullPointerException
+			else
+				tempNode = tempNode.next;
+			count++;
+		}
+
+	}
+
+}
+
