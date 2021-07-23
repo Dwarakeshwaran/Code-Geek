0089d68c2acf6626c5d5eedbc5f4fd158aee8952
dm826p <dm826p@att.com>
2021-07-23 15:44:14 +0530

    [LeetCode] RemoveNthNodeFromEndOfList - MyLinkedList and ListNode Classes separated

--- Java/Payirchi/src/dwarki/exercise/java/leetcode/linkedlist/RemoveNthNodeFromEndOfList.java
+++ Java/Payirchi/src/dwarki/exercise/java/leetcode/linkedlist/RemoveNthNodeFromEndOfList.java
@@ -7,100 +7,7 @@ import java.util.Arrays;
 
 //Started @16:43 21-07-2021
 //Ended @19:34 21-07-2021
-class Node {
-	int data;
-	Node nextNode;
 
-	public Node() {
-		super();
-	}
-
-	public Node(int data) {
-		super();
-		this.data = data;
-	}
-
-	public Node(int data, Node nextNode) {
-		super();
-		this.data = data;
-		this.nextNode = nextNode;
-	}
-
-}
-
-class MyLinkedList {
-
-	Node headNode;
-
-	public MyLinkedList() {
-		super();
-	}
-
-	public MyLinkedList(Node headNode) {
-		super();
-		this.headNode = headNode;
-	}
-
-	public void add(int number) {
-
-		Node duplicateNode = headNode;
-
-		if (headNode == null)
-			headNode = new Node(number);
-		else {
-			while (duplicateNode.nextNode != null)
-				duplicateNode = duplicateNode.nextNode;
-			duplicateNode.nextNode = new Node(number);
-		}
-
-	}
-
-	public void print() {
-
-		Node duplicateNode = headNode;
-
-		while (duplicateNode != null) {
-			System.out.print(duplicateNode.data + " ");
-			duplicateNode = duplicateNode.nextNode;
-		}
-	}
-
-	public void removeAtPosition(int position) {
-
-		Node duplicateNode = headNode;
-
-		int count = 1;
-
-		if (count == position) {//This is for deleting the first Element
-			headNode = headNode.nextNode;
-			return;
-		}
-
-		while (duplicateNode.nextNode != null) {
-			if ((count + 1) == position) {
-				duplicateNode.nextNode = duplicateNode.nextNode.nextNode;
-			}
-			if (count == position - 1)// This is for deleting the last element
-				return;// so that we can avoid the NullPointerException
-			else
-				duplicateNode = duplicateNode.nextNode;
-			count++;
-		}
-
-	}
-
-	public int size() {
-
-		Node duplicateNode = headNode;
-		int counter = 0;
-
-		while (duplicateNode != null) {
-			duplicateNode = duplicateNode.nextNode;
-			counter++;
-		}
-		return counter;
-	}
-}
 
 public class RemoveNthNodeFromEndOfList {
 
@@ -110,7 +17,7 @@ public class RemoveNthNodeFromEndOfList {
 
 		head.removeAtPosition(position);
 
-		head.print();
+		head.show();
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -125,7 +32,7 @@ public class RemoveNthNodeFromEndOfList {
 
 		for (int i : nodeArray)
 			list.add(i);
-
+		
 		removeNthFromEnd(list, position);
 
 	}
