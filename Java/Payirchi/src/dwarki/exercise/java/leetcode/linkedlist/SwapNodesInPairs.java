1d07c840a24ac56cb0bee8d0f007c89c3cf7d967
dm826p <dm826p@att.com>
2021-07-27 11:21:06 +0530

    [LeetCode] SwapNodesInPairs - Final Commit

--- Java/Payirchi/src/dwarki/exercise/java/leetcode/linkedlist/SwapNodesInPairs.java
+++ Java/Payirchi/src/dwarki/exercise/java/leetcode/linkedlist/SwapNodesInPairs.java
@@ -15,7 +15,7 @@ public class SwapNodesInPairs {
 
 		int[] intArray = Arrays.stream(reader.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
 
-		TempLinkedList list = new TempLinkedList();
+		MyLinkedList list = new MyLinkedList();
 
 		for (int i : intArray)
 			list.add(i);
@@ -26,13 +26,13 @@ public class SwapNodesInPairs {
 
 	public static ListNode swapPairs(ListNode head) {
 
-		TempLinkedList list = new TempLinkedList(head);
+		MyLinkedList list = new MyLinkedList(head);
 
 		list.swapAdjacentNodesInPairs();
 
 		head = list.getNode();
 
-		new TempLinkedList(head).show();
+		//new MyLinkedList(head).show();
 
 		return head;
 
@@ -40,76 +40,3 @@ public class SwapNodesInPairs {
 
 }
 
-class TempLinkedList {
-
-	ListNode headNode;
-
-	public TempLinkedList() {
-		super();
-	}
-
-	public TempLinkedList(ListNode headNode) {
-		super();
-		this.headNode = headNode;
-	}
-
-	public void add(int number) {
-
-		ListNode tempNode = headNode;
-
-		if (headNode == null) {
-			headNode = new ListNode(number);
-			return;
-		}
-
-		while (tempNode.next != null)
-			tempNode = tempNode.next;
-		tempNode.next = new ListNode(number);
-	}
-
-	public void show() {
-
-		ListNode tempNode = headNode;
-
-		while (tempNode.next != null) {
-			System.out.print(tempNode.val + " ");
-			tempNode = tempNode.next;
-		}
-		System.out.print(tempNode.val + " ");
-		System.out.println();
-	}
-
-	public ListNode getNode() {
-		return headNode;
-	}
-
-	public ListNode swapAdjacentNodesInPairs() {
-
-		ListNode tempNode = headNode;
-
-		if (headNode == null)
-			return null;
-		else if (headNode.next == null)
-			return headNode;
-		else if (tempNode.next != null) {
-
-			while (tempNode.next != null) {
-
-				int temp = tempNode.val;
-				tempNode.val = tempNode.next.val;
-				tempNode.next.val = temp;
-
-				tempNode = tempNode.next;
-
-				if (tempNode.next == null)
-					return tempNode;
-				else
-					tempNode = tempNode.next;
-			}
-
-			return tempNode;
-		} else
-			return null;
-	}
-
-}
