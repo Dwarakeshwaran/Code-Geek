ccb0c71a03ac02ff68fdbc7f0f5dfb5ec069c072
dm826p <dm826p@att.com>
2021-08-01 14:15:39 +0530

    [LeetCode] TrappingRainWater - July31CodeChallenge - Final Commit

--- Java/Payirchi/src/dwarki/exercise/java/leetcode/arrays/TrappingRainWater.java
+++ Java/Payirchi/src/dwarki/exercise/java/leetcode/arrays/TrappingRainWater.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-//Started @22:04 31-07-2021
-//Ended @22:49 31-07-2021
+//2nd Version
 public class TrappingRainWater {
 
 	public static void main(String[] args) throws IOException {
@@ -19,40 +18,55 @@ public class TrappingRainWater {
 	}
 
 	public static int trap(int[] height) {
-
-		int first = 0;
-
-		int last = height.length - 1;
-
-		int count = 0;
-
-		while (true) {
-
-			if (height[first] == 0)
-				first++;
-
-			if (height[last] == 0)
-				last--;
-
-			if (first == height.length || last == -1)
-				break;
-
-			while (height[first] != 0 && height[last] != 0) {
-				for (int inner = first; inner <= last; inner++) {
-					if (height[inner] == 0)
-						count++;
-					else
-						height[inner] = height[inner] - 1;
-				}
-			}
+		
+		int[] maxLeft = new int[height.length]; 
+		int[] maxRight = new int[height.length];
+		
+		maxLeft[0] = 0;
+		maxRight[height.length-1] = 0;
+		
+		int leftMax = height[0];
+		
+		//Finding maximum Left Side Value of the Current Array
+		for(int i=1; i<height.length; i++) {
+			
+			if(height[i] < leftMax)
+				maxLeft[i] = leftMax;
+			else
+				leftMax = height[i];	
+		}
+		
+		int rightMax = height[height.length-1];
+		
+		//Finding maximum Right Side Value of the Current Array
+		for(int i=height.length-2; i>=0; i--) {
+			
+			if(height[i] < rightMax)
+				maxRight[i] = rightMax;
+			else
+				rightMax = height[i];
 
 		}
+		
+		int trapWaterSum = 0;
+		
+		
+		//Current Bar Height Should be between it's maxLeft and maxRight
+		//and subtract it with the Minimum of maxLeft and maxRight to get the 
+		//Trapping capacity of that current bar.
+		for(int i=0; i< height.length; i++) {
+			
+			if(maxLeft[i] > height[i] && height[i] < maxRight[i])				
+				trapWaterSum += minimumOf(maxLeft[i], maxRight[i]) - height[i];
+			
+		}
+		
+		return trapWaterSum;
 
-//		for (int i : height)
-//			System.out.print(i + " ");
-
-		return count;
-
+	}
+	
+	public static int minimumOf(int number1, int number2) {
+		return number1<number2?number1:number2;
 	}
 
 }
