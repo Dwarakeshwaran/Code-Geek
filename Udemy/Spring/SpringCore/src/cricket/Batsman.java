5ee99f0962bd39d25406f59eddfac42c6a7c9bb9
dm826p <dm826p@att.com>
2021-08-19 14:57:34 +0530

    Setter and Constructor Injections Practiced

--- Udemy/Spring/SpringCore/src/cricket/Batsman.java
+++ Udemy/Spring/SpringCore/src/cricket/Batsman.java
@@ -1,30 +1,50 @@
 package cricket;
 
 public class Batsman {
-	
+
 	private String batsmanName;
 	private int runs;
 	private double average;
 
+	public Batsman() {
+		super();
+	}
+
+	public Batsman(String batsmanName, int runs, double average) {
+		super();
+		this.batsmanName = batsmanName;
+		this.runs = runs;
+		this.average = average;
+
+		System.out.println("Batsman [batsmanName=" + batsmanName + ", runs=" + runs + ", average=" + average + "]");
+	}
+
 	public String getBatsmanName() {
 		return batsmanName;
 	}
+
 	public void setBatsmanName(String batsmanName) {
 		this.batsmanName = batsmanName;
 	}
+
 	public int getRuns() {
 		return runs;
 	}
+
 	public void setRuns(int runs) {
 		this.runs = runs;
 	}
+
 	public double getAverage() {
 		return average;
 	}
+
 	public void setAverage(double average) {
 		this.average = average;
 	}
-	
-	
+
+	public String display() {
+		return "Batsman [batsmanName=" + batsmanName + ", runs=" + runs + ", average=" + average + "]";
+	}
 
 }
--- Udemy/Spring/SpringCore/src/cricket/Cricketer.java
+++ Udemy/Spring/SpringCore/src/cricket/Cricketer.java
@@ -6,14 +6,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Cricketer {
 
 	public static void main(String[] args) {
-		
+
 		ApplicationContext container = new ClassPathXmlApplicationContext("beans.xml");
-		
+
 		Batsman batsman = container.getBean("batsman", Batsman.class);
-		
+
 		System.out.println(batsman);
-		
-		((ClassPathXmlApplicationContext)container).close();
+
+		System.out.println(batsman.display());
+
+		((ClassPathXmlApplicationContext) container).close();
 
 	}
 
