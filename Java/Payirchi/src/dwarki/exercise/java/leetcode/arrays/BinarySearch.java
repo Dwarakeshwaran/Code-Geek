261dc5854e0e4b7d749315bd43e0a9305f749281
Dwarakeshwaran <dwaki98@gmail.com>
2021-09-06 16:55:42 +0530

    [Leetcode] BinarySearch - Done - Getting Runtime error

--- Java/Payirchi/src/dwarki/exercise/java/leetcode/arrays/BinarySearch.java
+++ Java/Payirchi/src/dwarki/exercise/java/leetcode/arrays/BinarySearch.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class BinarySearch {
+	static int position = 0;
 
 	public static void main(String[] args) throws IOException {
 
@@ -22,21 +22,35 @@ public class BinarySearch {
 
 	public static int search(int[] nums, int target) {
 
-		int middle = (int) Math.floor(nums.length / 2);
+		int left = 0;
+		int right = nums.length - 1;
 
-		if (middle == 0) {
-			System.out.println(target + " is not available in this array :(");
-			return 0;
-		}
+		int place = binarySearch(nums, left, right, target);
 
-		if (nums[middle] == target || nums[0] == target || nums[nums.length - 1] == target)
-			System.out.println(target + " is available in this array :)");
-		else if (nums[middle] > target)
-			search(IntStream.range(0, middle).map(i -> nums[i]).toArray(), target);
-		else if (nums[middle] < target)
-			search(IntStream.range(middle, nums.length - 1).map(i -> nums[i]).toArray(), target);
+		System.out.println(target + " exists in nums and its index is " + position);
+		return place;
 
-		return 0;
+	}
+
+	private static int binarySearch(int[] nums, int left, int right, int target) {
+
+		int middle = (left + right) / 2;
+
+		if (target < nums[middle])
+			binarySearch(nums, left, middle - 1, target);
+		else if (target > nums[middle])
+			binarySearch(nums, middle + 1, right, target);
+		else if (target == nums[left])
+			position = left;
+		else if (target == nums[right])
+			position = right;
+		else if (target == nums[middle])
+			position = middle;
+
+		if (middle == 0)
+			return -1;
+		else
+			return position;
 
 	}
 
