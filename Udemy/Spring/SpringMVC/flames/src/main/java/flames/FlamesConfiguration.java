d1f04d2da79d55e974532ca62613e89ba0a49f6c
Dwarakeshwaran <dwaki98@gmail.com>
2021-09-07 17:01:00 +0530

    Flames Project Started!
    Added Java Based Web.xml file by implementing WebApplicationInitializer interface.

---
+++ Udemy/Spring/SpringMVC/flames/src/main/java/flames/FlamesConfiguration.java
@@ -0,0 +1,16 @@
+package flames;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class FlamesConfiguration implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		System.out.println("Started!");
+		
+	}
+
+}
