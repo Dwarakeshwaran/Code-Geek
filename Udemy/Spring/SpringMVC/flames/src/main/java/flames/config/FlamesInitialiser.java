52b1fe743e59c12c02da1efd6a250f7e9b2c8e4b
Dwarakeshwaran <dwaki98@gmail.com>
2021-10-04 15:53:56 +0530

    Dispatcher Servlet Configured by extending AbstractAnnotationConfigDispatcherServletInitializer

--- Udemy/Spring/SpringMVC/flames/src/main/java/flames/config/FlamesInitialiser.java
+++ Udemy/Spring/SpringMVC/flames/src/main/java/flames/config/FlamesInitialiser.java
@@ -1,27 +1,46 @@
 package flames.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+/*
+ * public class FlamesInitialiser implements WebApplicationInitializer {
+ * 
+ * public void onStartup(ServletContext servletContext) throws ServletException
+ * { System.out.println("Started");
+ * 
+ * AnnotationConfigWebApplicationContext webAppContainer = new
+ * AnnotationConfigWebApplicationContext();
+ * webAppContainer.register(FlamesConfiguration.class);
+ * 
+ * DispatcherServlet dServlet = new DispatcherServlet(webAppContainer);
+ * 
+ * ServletRegistration.Dynamic customServlet =
+ * servletContext.addServlet("flamesServlet", dServlet);
+ * 
+ * customServlet.setLoadOnStartup(1); customServlet.addMapping("/home/*"); }
+ * 
+ * }
+ */
 
-public class FlamesInitialiser implements WebApplicationInitializer {
+public class FlamesInitialiser extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		System.out.println("Started");
-
-		AnnotationConfigWebApplicationContext webAppContainer = new AnnotationConfigWebApplicationContext();
-		webAppContainer.register(FlamesConfiguration.class);
-
-		DispatcherServlet dServlet = new DispatcherServlet(webAppContainer);
-
-		ServletRegistration.Dynamic customServlet = servletContext.addServlet("flamesServlet", dServlet);
-
-		customServlet.setLoadOnStartup(1);
-		customServlet.addMapping("/home/*");
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		@SuppressWarnings("rawtypes")
+		Class[] arr = { FlamesConfiguration.class };
+		return arr;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+
+		String[] arr = { "/home/*" };
+		return arr;
+	}
 }
