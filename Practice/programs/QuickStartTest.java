7b3cd5ff65c0759c3a3489f5db7bd59f53515d6e
Dwarakeshwaran <dwaki98@gmail.com>
2021-10-11 22:29:53 +0530

    Practice Updated

---
+++ Practice/programs/QuickStartTest.java
@@ -0,0 +1,14 @@
+package programs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QuickStartTest {
+
+	@Test
+	void testMain() {
+		assertTrue(true);
+	}
+
+}
